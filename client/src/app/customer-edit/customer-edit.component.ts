f7e07ca1c926637bc59702302dd0b93ff2d55998
Bruno Paulino <paulino.bruno@gmail.com>
2019-01-20 12:58:41 -0200

    :construction: add: working crud

--- client/src/app/customer-edit/customer-edit.component.ts
+++ client/src/app/customer-edit/customer-edit.component.ts
@@ -1,15 +1,59 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs/Subscription';
+import { CustomerService } from '../shared/customer/customer.service';
 
 @Component({
   selector: 'app-customer-edit',
   templateUrl: './customer-edit.component.html',
   styleUrls: ['./customer-edit.component.css']
 })
-export class CustomerEditComponent implements OnInit {
+export class CustomerEditComponent implements OnInit, OnDestroy {
+  customer: any = {};
+  sub: Subscription;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private customerService: CustomerService) {
+  }
 
   ngOnInit() {
+    this.sub = this.route.params.subscribe(params => {
+      const { id } = params;
+
+      if (id) {
+        this.customerService.get(id).subscribe((customer: any) => {
+          if (customer) {
+            this.customer = customer;
+          } else {
+            console.log(`Customer with id '${id}' not found, returning to list`);
+            this.gotoList();
+          }
+        });
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
+  }
+
+  gotoList() {
+    this.router.navigate(['/customer-list']);
+  }
+
+  save(form: NgForm) {
+    this.customerService.save(form).subscribe(_ => {
+      this.gotoList();
+    }, error => console.error(error));
+  }
+
+  remove(id) {
+    this.customerService.remove(id).subscribe(result => {
+      this.gotoList();
+    }, error => console.error(error));
   }
 
 }
