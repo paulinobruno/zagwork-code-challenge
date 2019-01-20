58ab619cfa4a9cea46c6d362d9d0711153a85fbd
Bruno Paulino <paulino.bruno@gmail.com>
2019-01-20 12:40:08 -0200

    :construction: add: customer edit

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,19 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-
-import { AppComponent } from './app.component';
-import { CustomerService } from './shared/customer/customer.service';
-import { CustomerListComponent } from './customer-list/customer-list.component';
-
 import { MatButtonModule, MatCardModule, MatInputModule, MatListModule, MatToolbarModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
+import { CustomerService } from './shared/customer/customer.service';
+import { CustomerListComponent } from './customer-list/customer-list.component';
+import { CustomerEditComponent } from './customer-edit/customer-edit.component';
+
+
+
+const appRoutes: Routes = [
+  { path: '', redirectTo: '/customer-list', pathMatch: 'full' },
+  {
+    path: 'customer-list',
+    component: CustomerListComponent
+  },
+  {
+    path: 'customer-add',
+    component: CustomerEditComponent
+  },
+  {
+    path: 'customer-edit/:id',
+    component: CustomerEditComponent
+  }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    CustomerListComponent
+    CustomerListComponent,
+    CustomerEditComponent
   ],
   imports: [
     BrowserModule,
@@ -24,6 +44,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     MatInputModule,
     MatListModule,
     MatToolbarModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [CustomerService],
   bootstrap: [AppComponent]
--- client/src/app/shared/customer/customer.service.ts
+++ client/src/app/shared/customer/customer.service.ts
@@ -7,11 +7,32 @@ import { Observable } from 'rxjs/Observable';
 })
 export class CustomerService {
 
+  public API = '//localhost:8080/customers';
+
   constructor(private http: HttpClient) {
   }
 
   getAll(): Observable<any> {
-    return this.http.get('//localhost:8080/customers');
+    return this.http.get(this.API);
+  }
+
+  get(id: string) {
+    return this.http.get(`${this.API}/${id}`);
+  }
+
+  save(customer: any): Observable<any> {
+    let result: Observable<Object>;
+
+    if (customer.id) {
+      result = this.http.put(`${this.API}/${customer.id}`, customer);
+    } else {
+      result = this.http.post(this.API, customer);
+    }
+    return result;
+  }
+
+  remove(id: string) {
+    return this.http.delete(`${this.API}/${id}`);
   }
 
 }
